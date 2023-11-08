80f5d578f5116452f61755b5726ce39f8aeaf531
AlexChabriel <100591867+AlexChabriel@users.noreply.github.com>
2023-11-08 16:19:10 +0100

    commit front

--- pages/page.php
+++ pages/page.php
@@ -3,7 +3,7 @@
 require '../sql/ConnectionSQL.php';
 require '../composants/PageManager.php';
 $pageManager=new PageManager($db);
-$page_data = $pageManager->getPageById(3);
+$page_data = $pageManager->getPageById($_GET['p']);
 $title = '';
 $content = '';
 
