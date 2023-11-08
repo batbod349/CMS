e02955d7abe95521d1e79a2fd665a9e13d4f0dad
AlexChabriel <100591867+AlexChabriel@users.noreply.github.com>
2023-11-08 11:25:16 +0100

    commit front page

--- pages/page.php
+++ pages/page.php
@@ -1,4 +1,23 @@
-<?php include('C:\laragon\www\CMS\includes\header.php');?>
+<?php
+ include('../includes/header.php');
+require '../sql/ConnectionSQL.php';
+require '../composants/PageManager.php';
+$pageManager=new PageManager($db);
+$page_data = $pageManager->getPageById(3);
+$title = '';
+$content = '';
+
+if (!empty($page_data)) {
+    $title = isset($page_data['title']) ? htmlspecialchars($page_data['title']) : '';
+    $content = isset($page_data['content']) ? htmlspecialchars($page_data['content']) : '';
+    $img = isset($page_data['image']) ? htmlspecialchars($page_data['image']) : '';
+    // Autres données à initialiser si nécessaire
+    
+} else {
+    echo "Page non trouvée.";
+}
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -9,6 +28,8 @@
 </head>
 <body>
     <div id="page">
+        <h1 style="text-align: center; margin: 0 auto;"><?php echo $title ?></h1>
+        <div><?php echo html_entity_decode($content) ?></div>
     </div>
 </body>
 </html>
