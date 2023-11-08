36ee01cf19ab6314172cb9e8f36ac3603194bd28
batbod349 <batbod349@gmail.com>
2023-11-08 17:03:31 +0100

    Squash

---
+++ admin/Pages.php
@@ -0,0 +1,32 @@
+<?php
+ include('../sql/ConnectionSQL.php');
+ $sql = 'SELECT * FROM page';   
+$req = $db->query($sql);   
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <table>
+    <thead>
+        <tr>
+        <th colspan="2">The table header</th>
+        </tr>
+    </thead>
+    <tbody>
+            <? while($row = $req->fetch()) { ?>
+     <tr>
+            <td><? echo $row['Id']; ?></td>
+            <td><? echo $row['Title']; ?></td>
+            <td><button name="modif"class="">modification</button></td>
+            <td><button name="supre" class="">supression</button></td>
+     </tr>
+        <? }   ?>
+    </tbody>
+    </table>
+</body>
+</html>
