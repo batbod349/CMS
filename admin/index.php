af5acff4bb62aa0848509a5cfbd61d04e2a4b253
lasuc <115156956+lasuc@users.noreply.github.com>
2023-11-08 15:25:37 +0100

    Page admin

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Admin</title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-</head>
-<body>
-    <!-- Le contenu de votre page -->
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Admin</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+    </head>
+    <body>
+        <div class="cadre-vide">
+            <p class='conteneur-avec-bordure'>Logo</p>
+            <p>Choisir votre logo</p>
+        </div>
+            <div class="liste-de-pages">
+            <p>Liste des pages</p>
+                <div class="liste_puce">
+                    <ul>
+                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li>
+                        <li><a href="#">Page 3</a></li>
+                        <li><a href="#">Page 4</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <p>Charte graphique</p>
+        <!-- Le contenu de votre page -->
+
+    </body>
 </html>
