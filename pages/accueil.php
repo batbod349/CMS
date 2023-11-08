4b9397426b6b7c860a39988e50c9a9e1f852e59c
batbod349 <batbod349@gmail.com>
2023-11-08 12:04:47 +0100

    Mise en forme

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,16 +1,9 @@
 <?php require '../includes/header.php'; ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Accueil</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    </head>
     <body>
 
         <?php require '../composants/ajoutComp.php'; ?>
 
     </body>
-</html>
+
+    <?php require '../includes/footer.php'; ?>
